2c2e23520d8253c4319568d1037bf90e2d53bd4b
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-27 13:46:32 -0700

    done task 7

---
+++ task7.dart
@@ -0,0 +1,13 @@
+void main() {
+  for (int i = 1; i <= 20; i++) {
+    print(i);
+  }
+
+ 
+
+  List devices = ["Laptob", "Phone", "watch"];
+
+  for (int i = 0; i < devices.length; i++) {
+    print(devices[i]);
+  }
+}
