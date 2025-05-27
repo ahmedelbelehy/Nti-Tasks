394d00d7a8331026a65850807c8173b56a342c56
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-27 13:40:09 -0700

    done task 6

---
+++ task6.dart
@@ -0,0 +1,13 @@
+void main() {
+  List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 11, 12, 13, 14];
+
+
+  for (int i = 0; i < numbers.length; i++) {
+    print(numbers[i]);
+  }
+
+  numbers.forEach((item)  => print(item));
+}
+
+
+ 
