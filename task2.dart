9d73e520175f4b151dac23a17a0f737a9c346bd3
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-27 12:54:08 -0700

    done the task 2.

---
+++ task2.dart
@@ -0,0 +1,30 @@
+void main() {
+  var name = "Elbelehy";
+  print("My name is $name ");
+
+
+  dynamic age = 20;
+  print(" my age is $age years old");
+
+  // name = 21;
+
+  age = "Twenty ";
+
+  print(
+      "the var cannot change the value in different types but the dynamic can change the value in different types.\n");
+
+
+  // final and const
+  final name1 = "Elbelehy";
+  print("My name is $name1 ");
+  
+  const age1 = 20;
+  print(" my age is $age1 years old");  
+  
+
+  // name1 = "elbelehy";
+
+  // age1 = 20;
+
+  print("final Value is assigned once; can be determined at runtime.\nconst Value is a compile-time constant (assigned once) and ensures deep immutability for objects/collections.");
+}
