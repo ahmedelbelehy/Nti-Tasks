14d2030337d1cc192ac679a986d14e5076889320
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-27 13:15:46 -0700

    done task 4

---
+++ task4.dart
@@ -0,0 +1,12 @@
+void main() {
+  List numbers = [];
+
+  numbers.addAll([2, 3, 4, 5, 6]);
+    print(numbers);
+
+  numbers.removeAt(0);
+    print(numbers);
+
+  numbers.removeLast();
+    print(numbers);
+}
