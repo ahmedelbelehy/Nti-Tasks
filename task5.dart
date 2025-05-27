f401688bb99663ff6b0a306b6357c8d505efd180
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-27 13:31:30 -0700

    done task 5

---
+++ task5.dart
@@ -0,0 +1,20 @@
+void main () {
+  
+List name = ['elbelehy','alaa','mohammed'];
+name.addAll(['ahmed','yasser']); 
+
+print(name);
+
+
+name.remove('alaa');  
+print(name);
+
+print(name.length);
+
+print(name.contains('elbelehy'));
+
+}
+
+
+
+
