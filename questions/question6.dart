a655cad068f7ebf177e9b6d650b8e320a48a91ee
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-28 03:08:08 -0700

    done 6

---
+++ questions/question6.dart
@@ -0,0 +1,12 @@
+void main (){
+
+final name = "Elbelehy";
+
+const age = 21;
+
+print('name is $name and age is $age');
+
+print("final is cannot be changed in different types but const can be changed in different types.");
+print("const is a compile-time constant (assigned once) and ensures deep immutability for objects/collections.");
+
+}
