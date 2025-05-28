6bf001de6759215bdfb6ed07afe49e28256021d9
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-28 03:00:56 -0700

    done 5

---
+++ questions/question5.dart
@@ -0,0 +1,8 @@
+void main() {
+  var age = 25;
+  age = 30;
+  print("Age: $age");
+                          //final is cannot be changed in different types but const can be changed in different types.
+                          // var is cannot be changed in different types but dynamic can be changed in different types.
+  
+}
