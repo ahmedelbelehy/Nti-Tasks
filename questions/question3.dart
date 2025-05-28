086eb5df500bbb44d7905bfb84198dca8ea091c6
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-28 02:48:12 -0700

    done3

---
+++ questions/question3.dart
@@ -0,0 +1,10 @@
+void main() {
+  dynamic x = 10;        
+  print(x);               
+
+  x = 'hello';            
+  print(x);               
+
+  x = [1, 2, 3];          
+  print(x);               
+}
