a28984ea313af81ef6447076f98b96fd096f7e5a
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-28 02:53:24 -0700

    done 4

--- questions/question4.dart
+++ questions/question4.dart
@@ -3,7 +3,7 @@ void main() {
 
 print("var is cannot be changed in different types but dynamic can be changed in different types.");
 
-peint("dynamic is flexible it can be any type.");
+print("dynamic is flexible it can be any type.");
 
 
 
