3701875a6c832da95752a9936adf137de3e7ba0b
ahmedelbelehy <148384197+ahmedelbelehy@users.noreply.github.com>
2025-05-28 02:38:31 -0700

    done

---
+++ questions/question1.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main(){
+
+  print("Enter your name team football");
+  String name = stdin.readLineSync()!;
+
+  print("Enter your age team football");
+  int age = int.parse(stdin.readLineSync()!);
+
+  print("Enter your country team football");
+  String country = stdin.readLineSync()!;
+
+  print("Enter your position team football");
+  String position = stdin.readLineSync()!;
+
+  print("Enter your number team football");
+  int number = int.parse(stdin.readLineSync()!);
+
+
+
+
+
+
+
+}
